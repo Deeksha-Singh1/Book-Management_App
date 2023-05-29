98f25709ab8388ccb5fc3565aeae5bd30924e934
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-05-30 00:56:38 +0530

    book management system

---
+++ client/src/screens/IssueRequest.js
@@ -0,0 +1,65 @@
+import React,{useEffect} from 'react';
+import {getAllBookIssueReq ,issuedReq,issuedReqDeletedByAdmin} from "../actions/Issue_action"
+import { useDispatch, useSelector } from 'react-redux'
+
+const IssueRequest = () => {
+
+    const dispatch = useDispatch() ;
+    
+    useEffect(()=> {
+        dispatch(getAllBookIssueReq())
+    },[dispatch])
+   
+   
+
+    const {issueBooks} = useSelector(state => state.getAllIssueBookReqReducer)
+    const newIssuedBook = issueBooks && issueBooks.filter(item => !item.isIssue && !item.isRecom)
+
+    return (
+        <div className="col-md-10 m-auto">
+             <p style={{fontFamily:"sans-serif",fontSize:"30px",textAlign:"center",padding:"10px",color:"wheat"}}>Student Requested to Admin to issue these Book</p>
+            <table  className='table table-bordered table-responsive-sm'>
+
+
+<thead className='thead-dark' style={{color:"white"}}>
+    <tr >
+        <th>Book Name</th>
+        <th>Author</th>
+        <th>Student Name</th>
+        <th>Student Branch</th>
+        
+    </tr>
+</thead>
+<tbody>
+    
+{newIssuedBook && newIssuedBook.map(book=>{
+
+    return <tr key={book._id} >
+        <td>{book.title}</td>
+        <td>
+            {book.author}
+        </td>
+        <td>
+            {book.userName}
+        </td>
+        <td>
+            {book.userBranch}
+        </td>
+       
+        <td>
+           
+             <button onClick={() => dispatch(issuedReq(book._id,book.bookId))} className="btn btn-success">Accepted</button> { "  "}
+             <button onClick={() => dispatch(issuedReqDeletedByAdmin(book._id))} className="btn btn-danger">Rejected</button>
+        </td>
+
+    </tr>
+
+})}
+</tbody>
+
+</table>
+        </div>
+    );
+};
+
+export default IssueRequest;
---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URI:
+        "mongodb://localhost:27017/myFirstDatabase",
+    JWT_SECRET: "MOHENMONDALJDFKFLL",
+};
