680eb525e52fe1214acd4564fe5bb3560145a5a1
Deeksha-Singh1 <singhdeeksha1511@gmail.com>
2023-06-01 20:17:29 +0530

    book management system

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     MONGO_URI:
-        "mongodb://localhost:27017/myFirstDatabase",
+        "mongodb://127.0.0.1:27017/myFirstDatabase",
     JWT_SECRET: "MOHENMONDALJDFKFLL",
 };
